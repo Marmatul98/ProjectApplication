d69ffec9ea29a33a906591071c0f9da90632219e
Marek Matula <R18439@student.osu.cz>
2020-02-03 23:05:55 +0100

    adding keywords to project work but is not nice

--- ProjectManager/Controllers/ProjectsController.cs
+++ ProjectManager/Controllers/ProjectsController.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using ProjectManager.DAL;
 using ProjectManager.Models;
+using ProjectManager.ViewModels;
 
 namespace ProjectManager.Controllers
 {
@@ -40,7 +42,11 @@ namespace ProjectManager.Controllers
         // GET: Projects/Create
         public ActionResult Create()
         {
+            var project = new Project();
             ViewBag.StudentID = new SelectList(db.Students, "StudentID", "FirstName");
+            ViewBag.YearID = new SelectList(db.Years, "YearID", "YearValue");
+            project.Keywords = new List<Keyword>();
+            PopulateAssignedKeywordData(project);
             return View();
         }
 
@@ -49,16 +55,26 @@ namespace ProjectManager.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ProjectID,Title,Description,Year,ProjectCourse,StudentID")] Project project)
+        public ActionResult Create([Bind(Include = "ProjectID,Title,Description,YearID,ProjectCourse,StudentID")] Project project, string[] selectedKeywords)
         {
+            if (selectedKeywords != null)
+            {
+                project.Keywords = new List<Keyword>();
+                foreach (var keyword in selectedKeywords)
+                {
+                    var keywordToAdd = db.Keywords.Find(int.Parse(keyword));
+                    project.Keywords.Add(keywordToAdd);
+                }
+            }
             if (ModelState.IsValid)
             {
                 db.Projects.Add(project);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-
+            PopulateAssignedKeywordData(project);
             ViewBag.StudentID = new SelectList(db.Students, "StudentID", "FirstName", project.StudentID);
+            ViewBag.YearID = new SelectList(db.Years, "YearID", "YearValue", project.YearID);
             return View(project);
         }
 
@@ -69,7 +85,12 @@ namespace ProjectManager.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Project project = db.Projects.Find(id);
+           // Project project = db.Projects.Find(id);
+            Project project = db.Projects
+            .Include(i => i.Keywords)
+            .Where(i => i.ProjectID == id)
+            .Single();
+            PopulateAssignedKeywordData(project);
             if (project == null)
             {
                 return HttpNotFound();
@@ -77,22 +98,95 @@ namespace ProjectManager.Controllers
             ViewBag.StudentID = new SelectList(db.Students, "StudentID", "FirstName", project.StudentID);
             return View(project);
         }
+        private void PopulateAssignedKeywordData(Project project)
+        {
+            var allKeywords = db.Keywords;
+            var projectKeywords = new HashSet<int>(project.Keywords.Select(k => k.KeywordID));
+            var viewModel = new List<AssignedKeyword>();
+            foreach (var keyword in allKeywords)
+            {
+                viewModel.Add(new AssignedKeyword
+                {
+                    KeywordID = keyword.KeywordID,
+                    Name = keyword.Name,
+                    Assigned = projectKeywords.Contains(keyword.KeywordID)
+                });
+            }
+            ViewBag.Keywords = viewModel;
+        }
 
         // POST: Projects/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ProjectID,Title,Description,Year,ProjectCourse,StudentID")] Project project)
+        public ActionResult Edit(int? id, string[] selectedKeywords)
         {
-            if (ModelState.IsValid)
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var projectToUpdate = db.Projects
+               .Include(i => i.Keywords)
+               .Where(i => i.ProjectID == id)
+               .Single();
+
+            if (TryUpdateModel(projectToUpdate, "", new string[] { "Title", "Description", "Year", "ProjectCourse", "Student" }))
+            {
+                try
+                {                 
+                    UpdateProjectKeywords(selectedKeywords, projectToUpdate);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                catch (RetryLimitExceededException)
+                {
+                
+                    ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
+                }
+            }
+            PopulateAssignedKeywordData(projectToUpdate);
+            return View(projectToUpdate);
+
+            /*if (ModelState.IsValid)
             {
                 db.Entry(project).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
             ViewBag.StudentID = new SelectList(db.Students, "StudentID", "FirstName", project.StudentID);
-            return View(project);
+            return View(project);*/
+        }
+
+        private void UpdateProjectKeywords(string[] selectedKeywords, Project projectToUpdate)
+        {
+            if (selectedKeywords == null)
+            {
+                projectToUpdate.Keywords = new List<Keyword>();
+                return;
+            }
+
+            var selectedKeywordsHS = new HashSet<string>(selectedKeywords);
+            var projectKeywords = new HashSet<int>
+                (projectToUpdate.Keywords.Select(p => p.KeywordID));
+            foreach (var keyword in db.Keywords)
+            {
+                if (selectedKeywordsHS.Contains(keyword.KeywordID.ToString()))
+                {
+                    if (!projectKeywords.Contains(keyword.KeywordID))
+                    {
+                        projectToUpdate.Keywords.Add(keyword);
+                    }
+                }
+                else
+                {
+                    if (projectKeywords.Contains(keyword.KeywordID))
+                    {
+                        projectToUpdate.Keywords.Remove(keyword);
+                    }
+                }
+            }
         }
 
         // GET: Projects/Delete/5
