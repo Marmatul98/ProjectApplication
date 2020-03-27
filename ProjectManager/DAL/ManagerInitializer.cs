63a4c23c90ea012822b29135960c7387f38c5a84
Marek Matula <R18439@student.osu.cz>
2020-03-27 13:29:05 +0100

    Keywords as filter works

--- ProjectManager/DAL/ManagerInitializer.cs
+++ ProjectManager/DAL/ManagerInitializer.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
 namespace ProjectManager.DAL
 {
     public class ManagerInitializer : System.Data.Entity.DropCreateDatabaseAlways<ManagerContext>
@@ -14,46 +13,35 @@ namespace ProjectManager.DAL
             {
                 new User{UserName = "admin", Password = "admin"}
             };
-
             users.ForEach(u => context.Users.Add(u));
             context.SaveChanges();
-
             var students = new List<Student>
            {
                new Student{FirstName = "Marek", LastName = "Matula", Email = "r18439@student.osu.cz", PersonalNumber = "R18439"},
                new Student{FirstName = "Jan", LastName = "Kripner", Email = "r18435@student.osu.cz", PersonalNumber = "R18435"}
            };
-
             students.ForEach(s => context.Students.Add(s));
             context.SaveChanges();
-
             var years = new List<Year>
             {
                 new Year{YearValue = 2019}
             };
-
             years.ForEach(y => context.Years.Add(y));
             context.SaveChanges();
-
-
             var keywords = new List<Keyword>
            {
                new Keyword{Name = "car"},
                new Keyword{Name=  "plane"}
            };
-
             keywords.ForEach(k => context.Keywords.Add(k));
             context.SaveChanges();
-
             var courses = new List<Course>
            {
                new Course{Name = "UMINT"},
                new Course{Name = "SOFTCO"}
            };
-
             courses.ForEach(c => context.Courses.Add(c));
             context.SaveChanges();
-
             var projects = new List<Project>
            {
                new Project{Title = "TestTitle" , Description = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Ut enim ad minim veniam, " +
@@ -66,7 +54,7 @@ namespace ProjectManager.DAL
                    CourseID = courses.Single(c => c.Name == "UMINT").CourseID,
                    YearID = years.Single( y => y.YearValue == 2019).YearID,
                    StudentID = students.Single( s => s.PersonalNumber == "R18439").StudentID, Keywords = new List<Keyword>()},
-               
+
                 new Project{Title = "TestTitle2" , Description = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Ut enim ad minim veniam, " +
                "quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." +
                "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. " +
@@ -77,24 +65,63 @@ namespace ProjectManager.DAL
                 CourseID = courses.Single(c => c.Name == "SOFTCO").CourseID,
                YearID = years.Single( y => y.YearValue == 2019).YearID,
                StudentID = students.Single( s => s.PersonalNumber == "R18435").StudentID, Keywords = new List<Keyword>()},
+
+
+                new Project{Title = "TestTitle3" , Description = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Ut enim ad minim veniam, " +
+               "quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." +
+               "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. " +
+               "Praesent dapibus. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus " +
+               "asperiores repellat. Vestibulum erat nulla, ullamcorper nec, rutrum non, nonummy ac, erat. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit " +
+               "aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Praesent id justo in neque elementum ultrices. " +
+               "Etiam posuere lacus quis dolor.",
+                   CourseID = courses.Single(c => c.Name == "UMINT").CourseID,
+                   YearID = years.Single( y => y.YearValue == 2019).YearID,
+                   StudentID = students.Single( s => s.PersonalNumber == "R18439").StudentID, Keywords = new List<Keyword>()},
+
+                new Project{Title = "TestTitle4" , Description = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Ut enim ad minim veniam, " +
+               "quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." +
+               "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. " +
+               "Praesent dapibus. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus " +
+               "asperiores repellat. Vestibulum erat nulla, ullamcorper nec, rutrum non, nonummy ac, erat. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit " +
+               "aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Praesent id justo in neque elementum ultrices. " +
+               "Etiam posuere lacus quis dolor.",
+                   CourseID = courses.Single(c => c.Name == "UMINT").CourseID,
+                   YearID = years.Single( y => y.YearValue == 2019).YearID,
+                   StudentID = students.Single( s => s.PersonalNumber == "R18439").StudentID, Keywords = new List<Keyword>()},
+
+                new Project{Title = "TestTitle5" , Description = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Ut enim ad minim veniam, " +
+               "quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." +
+               "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. " +
+               "Praesent dapibus. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus " +
+               "asperiores repellat. Vestibulum erat nulla, ullamcorper nec, rutrum non, nonummy ac, erat. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit " +
+               "aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Praesent id justo in neque elementum ultrices. " +
+               "Etiam posuere lacus quis dolor.",
+                   CourseID = courses.Single(c => c.Name == "UMINT").CourseID,
+                   YearID = years.Single( y => y.YearValue == 2019).YearID,
+                   StudentID = students.Single( s => s.PersonalNumber == "R18439").StudentID, Keywords = new List<Keyword>()},
+
+
            };
 
             projects.ForEach(p => context.Projects.Add(p));
             context.SaveChanges();
-
             AddKeyword(context, "TestTitle", "car");
             AddKeyword(context, "TestTitle", "plane");
             AddKeyword(context, "TestTitle2", "plane");
+            AddKeyword(context, "TestTitle3", "plane");
+            AddKeyword(context, "TestTitle3", "car");
+            AddKeyword(context, "TestTitle4", "plane");
+            AddKeyword(context, "TestTitle5", "car");
             context.SaveChanges();
 
 
         }
-            void AddKeyword(ManagerContext context, string projectTitle, string keywordName)
-            {
-                var prj = context.Projects.SingleOrDefault(p => p.Title == projectTitle);
-                var kwd = prj.Keywords.SingleOrDefault(k => k.Name == keywordName);
-                if (kwd == null)
-                    prj.Keywords.Add(context.Keywords.Single(k => k.Name == keywordName));
-            }
+        void AddKeyword(ManagerContext context, string projectTitle, string keywordName)
+        {
+            var prj = context.Projects.SingleOrDefault(p => p.Title == projectTitle);
+            var kwd = prj.Keywords.SingleOrDefault(k => k.Name == keywordName);
+            if (kwd == null)
+                prj.Keywords.Add(context.Keywords.Single(k => k.Name == keywordName));
         }
     }
+}
--- ProjectManager/ViewModels/AssignedKeyword.cs
+++ ProjectManager/ViewModels/AssignedKeyword.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
 namespace ProjectManager.ViewModels
 {
     public class AssignedKeyword
@@ -10,5 +9,22 @@ namespace ProjectManager.ViewModels
         public int KeywordID { get; set; }
         public string Name { get; set; }
         public bool Assigned { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is AssignedKeyword keyword &&
+                   KeywordID == keyword.KeywordID &&
+                   Name == keyword.Name &&
+                   Assigned == keyword.Assigned;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 1192755596;
+            hashCode = hashCode * -1521134295 + KeywordID.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Name);
+            hashCode = hashCode * -1521134295 + Assigned.GetHashCode();
+            return hashCode;
+        }
     }
 }
